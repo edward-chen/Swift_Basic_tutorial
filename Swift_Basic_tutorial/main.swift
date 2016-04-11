2a11900af05ed3fc1e75e66c21d80c995233704c
edward.chen <jacy22@gmail.com>
2016-04-11 11:18:53 +0800

    Initial Commit

---
+++ Swift_Basic_tutorial/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift_Basic_tutorial
+//
+//  Created by ChenYi-Hung on 2016/4/11.
+//  Copyright © 2016年 ChenYi-Hung. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
